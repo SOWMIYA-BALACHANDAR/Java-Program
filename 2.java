70220edcbc7f9b195d4579e83c4ecd3804c55486
SOWMIYA-BALACHANDAR <162460192+SOWMIYA-BALACHANDAR@users.noreply.github.com>
2024-03-18 22:14:13 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        // Declare a variable
+        int number =19;
+        
+        // Print the variable
+        System.out.println(number);
+	}
+}
