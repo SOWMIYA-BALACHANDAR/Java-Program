86bfca5b9137b4e4f91e886f2d786d48b3f89c89
SOWMIYA-BALACHANDAR <162460192+SOWMIYA-BALACHANDAR@users.noreply.github.com>
2024-03-19 15:37:52 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		double pi = 3.14;
+		double radius = 8.9;
+		double area = pi * radius * radius;
+		System.out.println("The Area of the given Circle is " + area);
+	}
+}
