35a8d7a7be3da651eba88a47cdd6ba6c32a7f46c
SOWMIYA-BALACHANDAR <162460192+SOWMIYA-BALACHANDAR@users.noreply.github.com>
2024-03-22 20:27:31 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        // your code goes here
+        String num1 = "25"; //You need to define "25" as a String and not int
+        String num2 = "69";
+        System.out.print(num1 + num2);
+    }
+}
