7568f88f7f13561f0b7f97451f94e0624a0dba63
SOWMIYA-BALACHANDAR <162460192+SOWMIYA-BALACHANDAR@users.noreply.github.com>
2024-03-22 20:25:52 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        // Solution as follows
+        String x = "Hello";
+        String y = "World";
+        System.out.print(x + " " + y);
+    }
+}
