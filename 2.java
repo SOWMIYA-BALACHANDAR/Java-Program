61591f74fee05126f163f553ac9eab1249ff3179
SOWMIYA-BALACHANDAR <162460192+SOWMIYA-BALACHANDAR@users.noreply.github.com>
2024-03-25 22:37:13 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,9 @@
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        String word = "Programming";
+        System.out.println(word.charAt(2));
+        System.out.print(word.charAt(1));
+    }
+}
